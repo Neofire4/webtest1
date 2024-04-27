b69671fb3b15d25442a8b6e347358608511a12d7
Benjamin W <benjay95@gmail.com>
2024-04-27 01:26:26 -0400

    Add files via upload
    refactor some code

--- proj/script.js
+++ proj/script.js
@@ -1,15 +1,10 @@
 "use strict";
 
-document.addEventListener ("DOMContentLoaded", (event) => {
-	setup();
-});
-
-
-
 function kickOff(context="all") {
 
 	const refresh = 5000;
-	const a_hosts = ["serverhost1","serverhost2","serverhost3"]
+	//const a_hosts = ["serverhost1","serverhost2","serverhost3"]
+	const a_hosts = ["serverhost1"]
 	const b_hosts = ["serverhost1","serverhost3"];
 	const b_modules = ["Message"];
 	
@@ -51,7 +46,7 @@ async function loop(interval,refresh=10000,context="all",hostlist=[],special_mod
 	//hostlist & holder element selector declarations
 	const counter = document.querySelector("#counter");
 	const holder = document.querySelector("#holder");
-
+	const timerIds = [];
 	//num used for the alert count
 	let num = Number(counter.innerText);
 
@@ -110,6 +105,9 @@ async function loop(interval,refresh=10000,context="all",hostlist=[],special_mod
 			//declaration for previous status holder elemnt
 			let prevstat = "";
 
+			const moduleCount = {"red":0,"green":0,"other":0,"total":(Object.keys(fetched[host]).length)};
+
+			//console.log(host, moduleCount);
 			//a boolean that changes to true if the overall host status has changed
 			let changed = false;
 			
@@ -122,12 +120,13 @@ async function loop(interval,refresh=10000,context="all",hostlist=[],special_mod
 			
 
 			//checks if previous comparison status node is present, if not it will create and append the node to the holder element
-			if (!(document.querySelector(host)) )
-				holder.appendChild(elementCreator(host,"","",""));
+			document.querySelector(host) ?? holder.appendChild(elementCreator(host,"","",""));
+				
 
+			prevstat = document.querySelector(host) 
 			
 			//shortend reference for host previous status element
-			prevstat = document.querySelector(host);
+			//prevstat = document.querySelector(host);
 
 
 			//loops through the each module of the host
@@ -136,13 +135,16 @@ async function loop(interval,refresh=10000,context="all",hostlist=[],special_mod
 				//shortened references to fetched data 
 				const mod = fetched[host][module];
 				const stats = fetched[host][module].status
-				
+				let modalert = false
+				let newredalert = false;
 				//makes the module name html compatible (removes spaces from name)
 				let statm = module.replaceAll(" ", "_");
 
 				//dynamically create a query selector for the previous status module selector
 				let qstr = `${host} > ${statm}`;
+			
 				let qnode = document.querySelector(qstr);
+
 				
 				
 				//alertmodule declaration, holds the module name
@@ -153,101 +155,98 @@ async function loop(interval,refresh=10000,context="all",hostlist=[],special_mod
 				if (qnode) {
 
 					//defines alermodule to new fetched status
-					alertmodule = elementCreator("div","module","",module,"data-status",mod.status.content,"data-time",mod.statustime,"data-nextrun",mod.NextRunTime)
+					//
 
-
-					//checks if the new status has changed from the previous, if true creates an alert if specific conditions are met
-					if ( !( (qnode.dataset.lasttime + qnode.dataset.laststatus) === (mod.statustime + mod.status.content) )  ){
+					if (mod.status.content === "FAILURE" && qnode.dataset.laststatus == "SUCCESS") {
+						console.log(host,module, " SUCCESS to FAILURE detected")
+						moduleCount["red"] += 1;
+						modalert = true;
+						newredalert = true;
+						newAlert++;
+					}
+					else if (mod.status.content === "FAILURE" && qnode.dataset.laststatus == "FAILURE") {
 						
-						if (!(mod.status.content === qnode.dataset.laststatus)) {
-							changed = true;
-							switch (mod.status.content) {
-								case "SUCCESS":
-									newAlert++;
-									break;
-								case "FAILURE":
-									overallstatus = 1
-									newAlert++;
-									break;
-								case "UNKNOWN":
-									overallstatus = 3
-									newAlert++;
-									break;
-								default:
-									overallstatus = 0
-									newAlert++;
-									break;
-							}
-						} else {
-							changed = true;
-							switch (mod.status.content) {
-								case "FAILURE":
-									overallstatus = 1
-									newAlert++;
-									break;
-								default:
-									console.log("default", module);
-									break;
-							}
+						if (mod.statustime === qnode.dataset.lasttime){
+							
+							console.log(host,module, "repeat FAILURE to FAILURE detected")
+						}else {
+							console.log(host,module, "new FAILURE to FAILURE time detected")
+							newAlert++;
+							newredalert = true;
 						}
+						moduleCount["red"] += 1;
+						modalert = true;
+	
 
-						alertitems.dataset.overall = overallstatus;
-						alertitems.appendChild(alertmodule);
+					} else if (mod.status.content === "SUCCESS" && qnode.dataset.laststatus == "FAILURE") {
+						console.log(host,module, " FAILURE to SUCCESS detected")
+						moduleCount["green"] += 1;
+						modalert = true;
+						newAlert++
+					} else {
+						console.log(host,module, ` ${qnode.dataset.laststatus} to ${mod.status.content} detected`)
+						moduleCount["other"] += 1;
 					}
 
-				
-				} 
 
-				//checks if previous status module to compare does not exist, then creates it and creates a new alert (this allows the initial alerts created on start up to display)
-				if (!(qnode) ) {
-					prevstat.appendChild( elementCreator(statm,"","","","data-laststatus",stats.content,"data-lasttime",mod.statustime) );
-					alertmodule = elementCreator("div","module","",module,"data-status",mod.status.content,"data-time",mod.statustime,"data-nextrun",mod.NextRunTime)
-					changed = true;
-					newAlert++;
-					
-					switch (mod.status.content) {
-						case "SUCCESS":
-							newAlert++;
-							break;
-						case "FAILURE":
-							overallstatus = 1
-							newAlert++;
-							break;
-						case "UNKNOWN":
-							overallstatus = 3
-							newAlert++;
-							break;
-						default:
-							console.log("new default", module);
-							overallstatus = 0
-							newAlert++;
-							break;
-					}
-					
-					alertitems.dataset.overall = overallstatus;
-					alertitems.appendChild(alertmodule);
-				} else {
-					//console.log("else qnode",host)
+
+
+					//console.log(host, moduleCount);
+					//update previous status for next run
 					qnode.dataset.laststatus = stats.content;
 					qnode.dataset.lasttime = mod.statustime;
-				}
+				
+				
+				
+				} else {
+					prevstat.appendChild( elementCreator(statm,"","","","data-laststatus",stats.content,"data-lasttime",mod.statustime) );
+					
+					document.querySelector(qstr).setAttribute("data-repeatfail", true)
+
+					if(mod.status.content === "FAILURE") {
+						console.log(host,module, " initial to FAILURE detected")
+						alertmodule = elementCreator("div","module","",module,"data-status",mod.status.content,"data-time",mod.statustime,"data-nextrun",mod.NextRunTime)
+						moduleCount["red"] += 1;
+						modalert = true;
+						newAlert++;
+						//prevstat.setAttribute("data-lastoverall",1);
+					}
+					//alertmodule = elementCreator("div","module","",module,"data-status",mod.status.content,"data-time",mod.statustime,"data-nextrun",mod.NextRunTime)
+				} 
+
+				
+				//if (modalert) {
+					alertmodule = elementCreator("div","module","",module,"data-status",mod.status.content,"data-time",mod.statustime,"data-nextrun",mod.NextRunTime)
+					alertmodule.setAttribute("data-newredalert", newredalert)
+					alertitems.append(alertmodule);
+					changed = true;
+				//}
 
 
 			}); //end module loop
 
 
 			//appends elements to eachother if there has been a change in status
-			if (changed) {
-				console.log("changed",host)
+			//if (changed) {
+				//console.log("changed",host)
+				if (moduleCount["red"] > 0 || moduleCount["red"] > moduleCount["green"]) {
+					prevstat.dataset.lastoverall == 1 ? alertitems.dataset.overall = 2 : alertitems.dataset.overall = 1
+/*						alertitems.dataset.overall = 2
+					else
+						alertitems.dataset.overall = 1;*/
+				}
+
 				alertitems.prepend(alerthost);
 				alertbody.appendChild(alertitems);
-			}
+
+				prevstat.setAttribute("data-lastoverall",alertitems.dataset.overall)
+			//}
 
 			
 		});// end host loop
 
 		//if newAlert counter is 1 or more, creates a new alertline to display
-		if (newAlert > 0) {
+		//if (newAlert > 0) {
 			alertline.prepend(alert_time);
 			alertline.appendChild(alertbody);
 			alertline.appendChild(alert_number);
@@ -257,7 +256,7 @@ async function loop(interval,refresh=10000,context="all",hostlist=[],special_mod
 			document.querySelector("#alert_holder").prepend(alertline);
 
 			counter.innerHTML = num + 1;
-		}
+		//}
 
 		
 
@@ -285,64 +284,36 @@ function setup() {
 	let  urlParams = new URLSearchParams(window.location.search);
 	let con = "";
 
+	console.log(urlParams.keys());
 
-	if (urlParams.getAll("context").length == 0) {
+	//checks for url parameters to set profile
+	switch(urlParams.getAll("context").length) {
 
+		//if no url param specified, display splash selection and add event listener to buttons to launch kickoff
+		case 0:
+			document.querySelectorAll(".context").forEach( (btn) => {
+				btn.addEventListener("click", () => {
+					btn.dataset.context.match(/^(A|B)$/) ? con = btn.dataset.context : con = "all";
+					document.querySelector("#welcome").className = "welcomeAni";
+					kickOff(con);
+				},{ once: true })
+			});
+			break;
+		//if valid url param specified, set profile to that param (A or B) 
+			case 1:
+			urlParams.getAll("context")[0].match(/^(A|B)$/) ? con = urlParams.getAll("context")[0] : con = "all";
+			welcome.style.display = "none";
+			kickOff(con);
+			break
+		//if invalid param is specified, defaults to all profile
+		default:
+			con = "all";
+			welcome.style.display = "none";
+			kickOff(con);
+			break
 
-		document.querySelectorAll(".context").forEach( (btn) => {
-			btn.addEventListener("click", () => {
-				switch (btn.dataset.context) {
-
-					case "A":
-						con = btn.dataset.context;
-						break;
-					case "B":
-						con = btn.dataset.context;
-						break;
-
-					default:
-						//console.log("invalid context detected:", btn.dataset.context, "setting to all")
-						con = "all";
-						break;
-				
-				}
-
-				document.querySelector("#welcome").className = "welcomeAni";
-				kickOff(con);
-
-			})
-
-
-		});
-
-	} else if (urlParams.getAll("context").length == 1) {
-		
-		switch (urlParams.getAll("context")[0]) {
-			
-			case "A":
-				con = urlParams.getAll("context")[0];
-				break;
-			case "B":
-				con = urlParams.getAll("context")[0];
-				break;
-
-			default:
-				//console.log("invalid context detected:", urlParams.getAll("context")[0], "setting to all")
-				con = "all";
-				break;
-			}
-		
-		
-		welcome.style.display = "none";
-		kickOff(con);
-	
-	} else {
-		//console.log("Invalid context detected:", urlParams.getAll("context")[0], "setting to all")
-		con = "all";
-		welcome.style.display = "none";
-		kickOff(con);
 	}
-
+	//appends the start time
 	document.querySelector("#start").innerText = `Start Time: ${st[0]}, ${st[1]} GMT`;
 }
 
@@ -367,10 +338,10 @@ function elementCreator(etype="div", eclass = "", eid = "", etext="",aname1="",a
 
 	if (!(eclass === "")) {newElement.className = eclass}
 	if (!(etext === "")) {newElement.innerText = etext}
-	if (!(eid === "")) {newElement.id = eid.replace(" ","_");}
-	if (!(aname1 === "" ) && !(aval1 === "" ) ) { newElement.setAttribute(aname1.replace(" ","_"),aval1) }
-	if (!(aname2 === "" ) && !(aval2 === "" ) ) { newElement.setAttribute(aname2.replace(" ","_"),aval2) }
-	if (!(aname3 === "" ) && !(aval3 === "" ) ) { newElement.setAttribute(aname3.replace(" ","_"),aval3) }
+	if (!(eid === "")) {newElement.id = eid.replaceAll(" ","_");}
+	if (!(aname1 === "" ) && !(aval1 === "" ) ) { newElement.setAttribute(aname1.replaceAll(" ","_"),aval1) }
+	if (!(aname2 === "" ) && !(aval2 === "" ) ) { newElement.setAttribute(aname2.replaceAll(" ","_"),aval2) }
+	if (!(aname3 === "" ) && !(aval3 === "" ) ) { newElement.setAttribute(aname3.replaceAll(" ","_"),aval3) }
 		
 			
 	return newElement
@@ -402,7 +373,7 @@ async function fetchAllData(urls=[]) {
 function fetchDataWithDelay(url, fetchdelay=100) {
     return new Promise((resolve) => {
         setTimeout(() => {
-            fetch(window.origin + `/${url}`)
+            fetch(window.origin + `/module/${url}`)
                 .then((response) => response.json())
                 .then((data) => {resolve(data)})
                 .catch((error) => {
@@ -414,4 +385,51 @@ function fetchDataWithDelay(url, fetchdelay=100) {
 }
 
 
+function createMe(name, attributes={},text=null) {
+
+    let ele = document.createElement(name);
+
+	if (text) {
+		ele.innerText = text;
+	}
+
+    if (Object.keys(attributes).length > 0) {
+        Object.keys(attributes).forEach((attr) => {
+			let an = "";let av = "";
+			switch (attr) {
+
+				case "class":
+					an = "class"
+					av = String(attributes[attr])
+					break
+				case "id":
+					an = "id";
+					av = String(attributes[attr]).replaceAll(" ", "_");
+					break
+				default:
+					an = String(attr).replaceAll(" ", "_");
+					av = String(attributes[attr]).replaceAll(" ", "_");
+					    
+					
+			}
+
+			ele.setAttribute(an,av);
+
+        })       
+    } else {
+        console.log("no attributes specified");
+    }
+ 
+	return ele
+}
+
+//When document loads, begin setup
+document.addEventListener ("DOMContentLoaded", () => {
+	
+	let x = createMe("div",{"class":"hello 123","data-numbers":"%", "data-dog":"true"})
+
+	document.body.appendChild(x);
+
+	setup();
+});
 
